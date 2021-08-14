638b620b6d7fdabdc07591b7372d1d92396967e0
Narendrakumar <kumawatnrndr291@gmail.com>
2021-08-14 16:37:05 +0530

    nothing much1

--- mysite_1/mysite_1/asgi.py
+++ mysite_1/mysite_1/asgi.py
@@ -33,3 +33,31 @@ application = ProtocolTypeRouter({
 # ..................................................................................................
 
 # However if you open a second browser tab to the same room page at http://127.0.0.1:8000/chat/lobby/ and type in a message, the message will not appear in the first tab. For that to work, we need to have multiple instances of the same ChatConsumer be able to talk to each other. Channels provides a channel layer abstraction that enables this kind of communication between consumers
+
+
+# import os
+
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.conf.urls import url
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite_1.settings")
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
+
+# from chat_1.consumers import AdminChatConsumer, PublicChatConsumer
+
+# application = ProtocolTypeRouter({
+#     # Django's ASGI application to handle traditional HTTP requests
+#     "http": django_asgi_app,
+
+#     # WebSocket chat handler
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter([
+#             url(r"^chat_1/admin/$", AdminChatConsumer.as_asgi()),
+#             url(r"^chat_1/$", PublicChatConsumer.as_asgi()),
+#         ])
+#     ),
+# })
